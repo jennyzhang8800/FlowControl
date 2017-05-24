1625ed5caa898daddbf1627acac6b61ec6e3a9fa
JennyZhang <jennyzhang8800@163.com>
2017-05-24 16:26:36 +0800

    通过参数查询MongoDB-封装成子流程

---
+++ 20170524-通过参数查询MongoDB-封装成子流程/queryMongo.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python 
+import sys
+from pymongo import MongoClient
+
+#connection build
+def connMongo(database_name,collection_name):
+    client = MongoClient("localhost",27017) #connect to MongoDB
+    db = client.get_database(database_name) #connect to database
+    col = db.get_collection(collection_name) #connect to collection
+    return col
+
+#Query
+def queryMongo(col,query_commend):
+    result = col.find_one(query_commend)
+    return result
+
+if __name__ == '__main__':
+    #get parameter from commend line
+    database_name = sys.argv[1]
+    collection_name = sys.argv[2]
+    query_commend = eval(sys.argv[3]) #invert string to dict
+
+    col = connMongo(database_name,collection_name)
+    result = queryMongo(col,query_commend)
+    print result
+    
